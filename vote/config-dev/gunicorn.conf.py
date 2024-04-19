e8dddbfce1c7a3f60628bb77520fac27d9cf9dca
williamlui00 <williamlui00@gmail.com>
2024-04-19 09:43:33 +0800

    add vote

---
+++ vote/config-dev/gunicorn.conf.py
@@ -0,0 +1,7 @@
+bind = "0.0.0.0:80"
+log_file = "-"
+accesslog = "-"
+workers = 2
+keepalive = 0
+loglevel = "debug"
+preload_app = True
---
+++ vote/config-prod/gunicorn.conf.py
@@ -0,0 +1,7 @@
+bind = "0.0.0.0:80"
+log_file = "-"
+accesslog = "-"
+workers = 4
+keepalive = 0
+loglevel = "info"
+preload_app = True
